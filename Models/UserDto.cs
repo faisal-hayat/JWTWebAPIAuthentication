60f108747e87cae65e8b1046a1ec0e06329c6518
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-10-26 01:52:32 +0500

    basic register function has been added

---
+++ Models/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtWebAPI.Models
+{
+    public class UserDto
+    {
+        // This class is for registeration
+        // This is where we will be adding class fields
+        public string UserName { get; set; } = string.Empty;
+        public string Passowrd { get; set; } = string.Empty;
+
+    }
+}
